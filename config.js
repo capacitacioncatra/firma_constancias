90c4f0eb58da18291a9e0155719754e8e8ec6241
Manue-777 <manuel.morales.avn@gmail.com>
2025-11-26 12:54:40 -0600

    Integracion con google shets

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // IMPORTANTE: Reemplaza esta URL con la URL de tu Google Apps Script
 const CONFIG = {
     // URL del Google Apps Script (la obtendrás después de implementar el script)
-    SHEETS_API_URL: 'TU_URL_AQUI',
+    SHEETS_API_URL: 'https://script.google.com/macros/s/AKfycbxkvshs_x57qiSwtb7GvZcIxqwuk4x3v94jCtgwO1MgbtDh40OEkFHU_aKDmOnZ32Ef/exec',
     
     // Puedes cambiar esto a false para volver a localStorage en desarrollo
     USE_GOOGLE_SHEETS: true
